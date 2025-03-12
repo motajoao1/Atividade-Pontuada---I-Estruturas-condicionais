adfc1781d9c9c86570d940fd045d117760ca0d39
João Paulo Mota <99102663+motajoao1@users.noreply.github.com>
2025-03-12 19:24:52 +0000

    salvar

---
+++ Lista_de_exercícios/questão_1.py
@@ -0,0 +1,15 @@
+import os
+os.system("clear")
+
+valorA = int(input("Digite o valor de A: " ))
+valorB = int(input("Digite o valor de B: " ))
+valorC = int(input("Digite o valor de C: " ))
+valor1 = int
+valor1 = valorA + valorB
+
+if valor1 < valorC:
+    print("A soma de A + B é menor que C")
+else: 
+    print("A + B é maior que C")
+
+
